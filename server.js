10b4c44d46c29e1fe0bb13d26d3f118f405d9c69
Charlie Cohen <charlie11cohen@gmail.com>
2022-05-08 02:48:59 +0100

    dwa

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use('/logout', (req, res) => {
   req.url
 
   res.clearCookie('session_token', { sameSite:'none', httpOnly:true, secure:true})
-  res.redirect(301, "https://google.com");
+  res.status(301).redirect('/')
   
 })
 
